79bcdc0108e9a560eca782d20659dc2b091f0b8a
Spandan Das Barman <spandandb94@gmail.com>
2023-04-30 21:33:58 +0530

    addition of backend

--- backend/plate_seeker/plate_seeker/views.py
+++ backend/plate_seeker/plate_seeker/views.py
@@ -1,21 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from django.conf import settings
+import os
+import numpy
 import cv2
 import numpy as np
 import pytesseract
 from tkinter import *
+import base64
 
-def get_number_plate(file_path):
+def get_number_plate(file):
     # command to run the pytesseract in order to convert the image text into python string
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
     # linking the haar cascade
-    cascade = cv2.CascadeClassifier("cascade\haar_cascade.xml")
+    cascade = cv2.CascadeClassifier(os.path.join(settings.BASE_DIR, "cascade\haar_cascade.xml"))
 
     # reading the input image
     # img = cv2.imread("Photos\cars5.jpg")
-    img = cv2.imread(file_path)
+    img = cv2.imdecode(numpy.fromstring(file.read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
 
     # so that the program does not proceed until any keyboard key is pressed
     cv2.waitKey(0)
@@ -42,27 +45,27 @@ def get_number_plate(file_path):
         plateROI = cv2.dilate(plateROI, kernel, iterations=1)
         plateROI = cv2.erode(plateROI, kernel, iterations=1)
 
-        # wait until  any key is pressed
-        cv2.waitKey(0)
-
     # using thresholding technique to convert it into a binary image
     (thresh, plate) = cv2.threshold(plateROI, 127, 255, cv2.THRESH_BINARY_INV)
 
-    cv2.imshow("plate", plate)
-    cv2.waitKey(0)
+    # Convert captured image to JPG
+    retval, buffer = cv2.imencode('.jpg', plate)
+
+    # Convert to base64 encoding and show start of data
+    jpg_as_text = base64.b64encode(buffer)
 
     # read the text on the plate
     read = pytesseract.image_to_string(plate, lang='eng')
     read = ''.join(e for e in read if e.isalnum())
-    print(read)
+    # print(read)
 
     # outlining the detected number plate on the original image
 
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 4)
     cv2.putText(img, read, (x, y-5), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
+    return jpg_as_text
 
 
 @api_view(['POST'])
 def get_details(request):
-    print(request.FILES)
-    return Response({"message": "Hello, world!"})
+    return Response({"image": get_number_plate(request.FILES['image'])})
